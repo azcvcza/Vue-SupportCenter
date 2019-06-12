aa0c6471a1c5d6b2f1e16914c2cf19d1e98d4a98
韦粤信 <283940802@qq.com>
2019-06-12 16:45:53 +0800

    router-link add exact

--- spcenter/src/components/NavMenu.vue
+++ spcenter/src/components/NavMenu.vue
@@ -1,6 +1,12 @@
 <template>
 <nav class="menu">
-	<router-link to="/">Home</router-link>
+	<router-link to="/" exact>Home</router-link>
 	<router-link to="FAQ">F.A.Q</router-link>
 </nav>
 </template>
+<style lang="stylus" scoped>
+@import "../style/imports";
+.router-link-active{
+	border-bottom-color:$primary-color;
+}
+</style>
