9c79d01f34f1f29b4919dd8d5c04359a21e8149f
韦粤信 <283940802@qq.com>
2019-06-13 03:22:47 +0800

    feat:build simple faq

--- spcenter/src/components/FAQ.vue
+++ spcenter/src/components/FAQ.vue
@@ -1,5 +1,36 @@
 <template>
 	<main class="faq">
 		<h1>Frequently Asked Question</h1>
+		<div class="error" v-if="error">Can't load the question</div>
+		<section class="list">
+			<article :key=question.id v-for="question of questions">
+				<h2 v-html="question.title"></h2>
+				<p v-html="question.content"></p>
+			</article>
+		</section>
 	</main>
 </template>
+<script>
+	export default {
+		data () {
+			return {
+				questions: [],
+				error: null
+			}
+		},
+		created () {
+			fetch('http://localhost:3000/questions')
+				.then(response => {
+				if (response.ok) {
+					return response.json()
+				} else {
+					return Promise.reject(new Error('error'))
+				}
+			}).then(result => {
+				this.questions = result
+			}).catch(e => {
+				console.log('error:', e)
+			})
+		}
+	}
+</script>
