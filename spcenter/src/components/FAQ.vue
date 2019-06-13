ff089ea0c9f00ac43c500bee7db1251c7268b4f4
韦粤信 <283940802@qq.com>
2019-06-13 20:36:48 +0800

    disable eslint and add remotedata

--- spcenter/src/components/FAQ.vue
+++ spcenter/src/components/FAQ.vue
@@ -1,10 +1,11 @@
+/* eslint-disable no-tabs */
 <template>
 	<main class="faq">
 		<h1>Frequently Asked Question</h1>
 		<div class="error" v-if="error">Can't load the question</div>
 		<Loading v-if="loading"/>
 		<section class="list">
-			<article :key="question.id" v-for="question of questions">
+			<article :key="question.id" v-for="question of questionList">
 				<h2 v-html="question.title"></h2>
 				<p v-html="question.content"></p>
 			</article>
@@ -12,39 +13,29 @@
 	</main>
 </template>
 <script>
+	import RemoteData from "../mixins/RemoteData";
 	export default {
-		data () {
+		mixins: [
+			RemoteData({
+				questionList: 'questions'
+			})
+		],
+		data() {
 			return {
 				questions: [],
 				error: null,
 				loading: false
-			}
+			};
 		},
-		async created () {
-			this.loading = true
+		async created() {
+			this.loading = true;
 			try {
-				this.questions = await this.$fetch('questions')
-				console.log('in faq', this.questions)
-				/*
-	      await fetch('http://localhost:3000/questions')
-	        .then(response => {
-	          if (response.ok) {
-	            var result = response.json()
-	            console.log('fetch success', result)
-							//	this.questions = response.json();
-	            return result
-	          } else {
-	            return Promise.reject(new Error('error'))
-	          }
-	        })
-	        .then(result => {
-	          console.log('result:', result)
-	          this.questions = result
-	        }) */
+				this.questions = await this.$fetch("questions");
+				console.log("in faq", this.questions);
 			} catch (e) {
-				this.error = e
+				this.error = e;
 			}
-			this.loading = false
+			this.loading = false;
 		}
-	}
+	};
 </script>
