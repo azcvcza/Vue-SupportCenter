ec20f17316c0d2e1c0a5bd24987a04d949b63c8e
韦粤信 <283940802@qq.com>
2019-06-13 21:09:26 +0800

    create smartform and register

--- spcenter/src/global-component.js
+++ spcenter/src/global-component.js
@@ -1,5 +1,6 @@
 /* eslint-disable eol-last */
 import Vue from 'vue'
 import Loading from './components/Loading'
-
+import SmartForm from './components/SmartForm'
 Vue.component('Loading', Loading)
+Vue.component('SmartForm', SmartForm)
