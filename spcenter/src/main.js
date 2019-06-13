de499b5dd693621c6eb7b936f3961b0b4f0045fc
韦粤信 <283940802@qq.com>
2019-06-13 20:49:46 +0800

    add state:user for auth

--- spcenter/src/main.js
+++ spcenter/src/main.js
@@ -6,10 +6,13 @@ import AppLayout from './components/AppLayout'
 import './global-component'
 import router from './router'
 import VueFetch from './plugins/fetch'
-
+import state from './state'
+import VueState from './plugins/state'
 Vue.use(VueFetch, { baseUrl: 'http://localhost:3000/' })
+Vue.use(VueState, state)
 new Vue({
     el: '#app',
     render: h => h(AppLayout),
-    router
+    router,
+    data: state
 })
