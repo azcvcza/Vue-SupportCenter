291824591e3da5cdc3e2474cf3931ab939abc85f
韦粤信 <283940802@qq.com>
2019-06-13 21:23:59 +0800

    make forminput and register

---
+++ spcenter/src/components/FormInput.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="row">
+		<input
+			type="text"
+			class="row"
+			:class="input"
+			:name="name"
+			:type="type"
+			:value.prop="value"
+			:placeholder="placeholder"
+		>
+	</div>
+</template>
+<script>
+	export default {
+		props: {
+			name: {
+				type: String
+			},
+			type: {
+				type: String,
+				default: "text"
+			},
+			placeholder: {
+				type: String
+			},
+			invalid: {
+				type: Boolean,
+				default: false
+			}
+		}
+	};
+</script>
